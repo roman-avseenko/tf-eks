a213910e68fd25d130ce7dc44c623bf6c462f2a2
Roman Avseenko <Raman.Auseyenka@itechart-group.com>
2021-09-20 23:13:33 +0300

    Merge different files

--- ingress_controller.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "ingress_controller" {
-  name             = "ingress-controller"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  create_namespace = true
-  namespace        = "ingress-nginx"
-
-  depends_on = [module.eks_cluster]
-}
--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
+terraform {
+  required_version = ">=1.0.0"
+
+  backend "remote" {
+    organization = "faiiinttt-tf-organization"
+    workspaces {
+      name = "tf-eks"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.48.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~>2.4.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~>2.2.0"
+    }
+  }
+}
+
+#Defining providers specification
 provider "aws" {
   region = var.region
 
